4527427429575fc8d136c9911a8a30cfa53cf63b
Amna Shafiq <amna.shafiq@devsinc.com>
2023-09-14 17:03:34 +0500

    added templates, views and urls

--- investify/settings.py
+++ investify/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'investify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ["store/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ store/views/__init__.py
@@ -0,0 +1,7 @@
+from .cart import Cart
+from .orders import OrderView
+from .signup import Signup
+from .orders import Order
+from .login import Login
+from .home import Index
+from .checkout import CheckOut
